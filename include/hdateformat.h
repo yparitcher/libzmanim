1c56f3d9f1aa2e912f9262612ecf9a425537cb87
yparitcher <yparitcher@gmail.com>
2019-01-24 17:29:24 -0500

    hdateformat: docs

--- include/hdateformat.h
+++ include/hdateformat.h
@@ -1,26 +1,32 @@
 /****
 Copyright (c) 2018 Y Paritcher
 
-This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser GeneralPublic License as published by the Free Software Foundation; 
+This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; 
 either version 2.1 of the License, or (at your option)any later version.
 
 This library is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; 
-without even the impliedwarranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 See the GNU Lesser General Public License for more details. 
 You should have received a copy of the GNU Lesser General Public License along with this library; 
-if not, write tothe Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA, 
+if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA, 
 or connect to: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
 ****/
 
 #ifndef HDATE_FORMAT_H
 #define HDATE_FORMAT_H
 
+//convert an int to a Hebrew char based representation. 5779 becomes תשע״ט
 char* numtohchar(int innum);
+// convert int based Hebrew month (hdate.month) to char based representation.
 const char* numtohmonth(int month, int leap);
+// convert int based Hebrew weekday (hdate.wday) to char based representation.
 const char* numtowday(hdate date);
+// convert hdate to char based representation.
 char* hdateformat(hdate *date);
 
+// convert parshah enum to char based representation.
 const char* parshahformat(parshah current);
+// convert yomtov enum to char based representation.
 const char* yomtovformat(yomtov current);
 
 #endif
